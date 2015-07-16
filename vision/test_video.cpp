f792ee2e569be6734308a87f758e56761838970e
yuantailing <yuantailing@126.com>
2015-07-16 12:15:59 +0800

    鼠标点击时打印坐标

--- vision/test_video.cpp
+++ vision/test_video.cpp
@@ -8,6 +8,11 @@ using namespace std;
 using namespace cv;
 
 #if 0
+void on_mouse(int event, int x, int y, int, void *) {
+    if(event == CV_EVENT_LBUTTONDOWN)
+        cout << y << '\t' << x << endl;
+}
+
 int main() {
     VideoCapture cap(1);
     if (!cap.isOpened())
@@ -21,6 +26,8 @@ int main() {
         vis->input(frame);
         vis->get_edge_white();
         imshow("window", vis->gen_as_pic());
+        cvSetMouseCallback("window0", on_mouse, 0);
+        cvSetMouseCallback("window", on_mouse, 0);
         if (waitKey(30) >= 0) break;
     }
     delete vis;
