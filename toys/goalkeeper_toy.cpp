3d9506470e1079166599c7c47273ce47d7eaf162
huanglijian <huanglijian1005@163.com>
2015-08-05 15:48:44 +0800

    守门员

--- toys/goalkeeper_toy.cpp
+++ toys/goalkeeper_toy.cpp
@@ -11,7 +11,7 @@ int main() {
     VideoCapture capture2(1);
     Transform trans2(2);
     Location location(motor);
-    location.add_camera(&capture2, &trans2);
+    location.add_camera(&capture2, &trans2, true);
     location.set_current_location(cv::Vec2d(0, 0), cv::Vec2d(-1, 0));
     while (1) {
         Vec2d loc = location.get_location().first;
@@ -19,12 +19,16 @@ int main() {
         Vec2d ball_loc = location.get_ball().second;
         cout << "ball:" << ball_loc << endl;
         imshow("location", location.gen_ground_view());
-        /*if (ball_loc[0] <= 350 || ball_loc[0] >= 1350) {
+        if (ball_loc[0] == 0 && ball_loc[1] == 0) {
             motor->stop();
-        } else */if (loc[0] - ball_loc[0] < 0) {
-            motor->go(5000, -0.5);
+        } else if (abs(ball_loc[0] - loc[0]) > 2000 || abs(ball_loc[1] - loc[1]) > 2000) {
+            motor->stop();
+        } else if (loc[0] - ball_loc[0] < 0) {
+            if (loc[0] > 700) motor->stop();
+            else motor->go(5000, -0.5);
         } else if (loc[0] - ball_loc[0] > 0) {
-            motor->go(5000, 0.5);
+            if (loc[0] < -700) motor->stop();
+            else motor->go(5000, 0.5);
         } else {
             motor->stop();
         }
